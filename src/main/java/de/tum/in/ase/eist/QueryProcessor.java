e87b623b0e52a693d80e4542a83e61ea531c35be
Yahya Naveed Saleem <ge32yal@mytum.de>
2022-07-15 11:07:52 +0200

    .

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -2,20 +2,52 @@ package de.tum.in.ase.eist;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 @Service
 public class QueryProcessor {
 
 
     public String process(String query) {
 		query = query.toLowerCase();
+        if(query.contains("what")) {
+            String[] strings = query.split("%20");
+            Pattern pattern = Pattern.compile("[^0-9]");
+            String regex = "\\d+";
+            List<String> list = new ArrayList();
+            for (String s : strings) {
+                if (s.matches(regex)) ;
+                {
+                    String numberOnly = pattern.matcher(s).replaceAll("");
+                            list.add(s);
+                }
+            }
+        }
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "yahya";
-        } else if(query.contains("what is 11 plus 0")){
-            return "11";
+        }    if(query.contains("what")) {
+            String[] strings = query.split("%20");
+            Pattern pattern = Pattern.compile("[^0-9]");
+            String regex = "\\d+";
+         //   List<String> list = new ArrayList();
+            String lastArray = "what is ";
+            for (String s : strings) {
+                if (s.matches(regex)) ;
+                {
+                    lastArray += " ";
+                    String numberOnly = pattern.matcher(s).replaceAll("");
+                  //  list.add(s);
+                    lastArray += s ;
+                }
+            }
+
+           return lastArray;
         }
         else { // TODO extend the programm here
             return "";
