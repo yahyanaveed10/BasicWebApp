68496ce28958652e9def62a1d92ededaababbafd
Yahya Naveed Saleem <ge32yal@mytum.de>
2022-07-15 10:20:25 +0200

    .

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,9 +13,9 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "MyTeam";
+           return "yahya";
         } else { // TODO extend the programm here
-            return "yahya";
+            return "";
         }
     }
 }
