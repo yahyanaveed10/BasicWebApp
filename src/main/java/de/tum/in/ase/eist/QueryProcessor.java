61c4c53309e7abae091cbec0b45f8679789681e9
Yahya Naveed Saleem <ge32yal@mytum.de>
2022-07-15 10:23:52 +0200

    .

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,7 +12,7 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("name")) {
+        } else if (query.contains("What is your name")) {
            return "yahya";
         } else { // TODO extend the programm here
             return "";
